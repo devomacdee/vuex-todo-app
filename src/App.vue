49dcc9903c8b2ecdda325038ea5e868a2e3fbfde
Devin McCoy <devingmccoy@gmail.com>
2017-05-18 08:47:52 -0400

    started my vuex todo app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/GetTodo.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div id="get-todo" class="container">
+    <input type="text"
+           class="form-control"
+           @change="getTodo"
+           placeholder="I need to do...">
+    <button class="btn btn-primary"
+            @click="addTodo">
+      Add
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    getTodo (e) {
+      this.$store.dispatch('getTodo', e.target.value)
+    },
+    addTodo () {
+      this.$store.dispatch('addTodo')
+      this.$store.dispatch('clearTodo')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
